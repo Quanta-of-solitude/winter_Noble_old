096b694a606e41140266b41acc9c7391a18bbd99
Quanta-of-solitude <34452724+Quanta-of-solitude@users.noreply.github.com>
2018-02-09 22:25:39 +0530

    reports

--- cogs/datas.py
+++ cogs/datas.py
@@ -166,5 +166,80 @@ class Datas:
                     break
                 await ctx.send(f'```js\n{page}\n```')
 
+    @commands.command()
+    async def report(self,ctx, *,rep:str = None):
+        if rep == None:
+            await ctx.send("`Nothing provided!`")
+            return
+        else:
+            url_rep = "{}".format(os.environ.get("reported_reports"))
+            url_list = "{}".format(os.environ.get("reported_renames"))
+            name = myjson.get(url_list)
+            name = json.loads(name)
+            data = myjson.get(url_rep)
+            data = json.loads(data)
+            UnId = str(uuid.uuid4())[:8]
+            data[f"{UnId}"] = {}
+            data[f"{UnId}"]["report"] = rep
+            data[f"{UnId}"]["by"] = "{}".format(ctx.author)
+            data[f"{UnId}"]["auth_id"] = "{}".format(ctx.author.id)
+            nameandid = "ID: "+UnId+"   "+"by "+"{}".format(ctx.author.name)
+            name.append(nameandid)
+            url_rep = myjson.store(json.dumps(data),update=url_rep)
+            url_list = myjson.store(json.dumps(name),update=url_list)
+            await ctx.send("Thank you very much for the report/suggestion! :smile:")
+
+    @commands.command()
+    @commands.check(have_permissions)
+    async def allreports(self, ctx):
+        '''loads all reports'''
+        try:
+            url_list = "{}".format(os.environ.get("reported_renames"))
+            name = myjson.get(url_list)
+            items = json.loads(name)
+            added_items = '\n'.join(items)
+            try:
+                out = await ctx.send("**Reports:**\n"+"```{}```".format(added_items))
+            except:
+                paginated_text = ctx.paginate(added_items)
+                for page in paginated_text:
+                    if page == paginated_text[-1]:
+                        out = await ctx.send(f'```\n{page}\n```')
+                        break
+                    await ctx.send(f'```\n{page}\n```')
+        except Exception as e:
+            print("`Error: {}`".format(e))
+            await ctx.send("`Error: List Not Found!`")
+
+    @commands.command(aliases = ["inforeport"])
+    @commands.check(have_permissions)
+    async def inforeports(self, ctx, *,code:str = None):
+        '''report infos'''
+        try:
+            if code == None:
+                await ctx.send("`Error: Missing ID!`")
+                return
+            url_rep = "{}".format(os.environ.get("reported_reports"))
+            data = myjson.get(url_rep)
+            data = json.loads(data)
+            report = data[f"{code}"]["report"]
+            author = data[f"{code}"]["by"]
+            author_id = data[f"{code}"]["auth_id"]
+            em = discord.Embed()
+            em.set_author(name = "Reports:", icon_url = "https://loading.io/spinners/recycle/lg.recycle-spinner.gif")
+            em.add_field(name = "By:", value = author,inline = False)
+            em.add_field(name = "Report Code:", value = code,inline = False)
+            em.add_field(name = "Author ID:", value = author_id,inline = False)
+            em.add_field(name = "Report:", value = report,inline = False)
+            em.set_thumbnail(url = "https://i.pinimg.com/736x/0c/79/0a/0c790a5a37da132923a79f853602a36a.jpg")
+            em.set_footer(text = "|Winter-Song| Report Tracker.", icon_url = self.bot.user.avatar_url)
+            em.color=discord.Colour.blue()
+            await ctx.send(embed = em)
+
+        except Exception as e:
+            print(e)
+            await ctx.send("`Error: No such report exist!`")
+
+
 def setup(bot):
 	bot.add_cog(Datas(bot))
