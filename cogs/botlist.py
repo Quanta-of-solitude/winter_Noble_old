cb71606229fefe71510d72556b6cb9a58758e1fe
Quanta-of-solitude <34452724+Quanta-of-solitude@users.noreply.github.com>
2018-03-20 04:38:34 +0530

    Update botlist.py

--- cogs/botlist.py
+++ cogs/botlist.py
@@ -13,6 +13,7 @@ class DiscordBotsOrgAPI:
 
     def __init__(self, bot):
         self.bot = bot
+        self.bot.loop.create_task(self.update_stats())
 
 
     async def update_stats(self):
@@ -20,11 +21,11 @@ class DiscordBotsOrgAPI:
         headers = {'Authorization' : dbltoken}
         while True:
             urlbot = 'https://discordbots.org/api/bots/385681784614027265/stats'
-            payload = {'server_count': len(bot.guilds)}
+            payload = {'server_count': len(self.bot.guilds)}
             async with aiohttp.ClientSession() as session:
                 async with session.post(urlbot, data = json.dumps(payload), headers = headers) as r:
                     if r.status == 200:
-                        pass
+                        print("POSTED")
                 await session.close()
             await asyncio.sleep(3600)
 
