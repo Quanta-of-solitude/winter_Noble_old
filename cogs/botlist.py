4f345dcf290c33a4c6cbc2f328c92ff847b45a96
Quanta-of-solitude <34452724+Quanta-of-solitude@users.noreply.github.com>
2018-03-20 03:35:32 +0530

    ffs

--- cogs/botlist.py
+++ cogs/botlist.py
@@ -1,33 +1,35 @@
-import os
+import dbl
 import discord
 from discord.ext import commands
+import os
 import aiohttp
 import asyncio
 import logging
 
-class BTList:
+
+class DiscordBotsOrgAPI:
+    """Handles interactions with the discordbots.org API"""
+
     def __init__(self, bot):
         self.bot = bot
+        self.token = '{}'.format(os.environ.get("dbltokenT"))
+        self.dblpy = dbl.Client(self.bot, self.token)
         self.bot.loop.create_task(self.update_stats())
+
     async def update_stats(self):
-        dbltoken = '{}'.format(os.environ.get("dbltokenT"))
-        headers = {'Authorization' : dbltoken}
+        """This function runs every 30 minutes to automatically update your server count"""
+
         while True:
             logger.info('attempting to post server count')
-            urlbot = 'https://discordbots.org/api/bots/{}/stats'.format(os.environ.get("Whynonid"))
-            payload = {'server_count': len(self.bot.guilds)}
-            async with aiohttp.ClientSession() as session:
-                async with session.post(urlbot, data = payload, headers = headers) as r:
-                    if r.status == 200:
-                        print("K")
-                        logger.info('posted server count ({})'.format(len(self.bot.guilds)))
-                    else:
-                        logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
-                    await session.close()
-                await asyncio.sleep(1800)
+            try:
+                await self.dblpy.post_server_count()
+                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
+            except Exception as e:
+                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+            await asyncio.sleep(1800)
 
 
 def setup(bot):
     global logger
     logger = logging.getLogger('bot')
-    bot.add_cog(BTList(bot))
+    bot.add_cog(DiscordBotsOrgAPI(bot))
