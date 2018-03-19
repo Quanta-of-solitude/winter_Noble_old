0c8a234403f0d9b0a5da060f2435498b5018f8c9
Quanta-of-solitude <34452724+Quanta-of-solitude@users.noreply.github.com>
2018-03-20 04:37:22 +0530

    FFS

--- cogs/botlist.py
+++ cogs/botlist.py
@@ -2,7 +2,7 @@ import os
 import dbl
 import discord
 from discord.ext import commands
-
+import json
 import aiohttp
 import asyncio
 import logging
@@ -13,20 +13,19 @@ class DiscordBotsOrgAPI:
 
     def __init__(self, bot):
         self.bot = bot
-        self.token = '{}'.format(os.environ.get("dbltokenT"))  #  set this to your DBL token
-        self.dblpy = dbl.Client(self.bot, self.token)
-        self.bot.loop.create_task(self.update_stats())
+
 
     async def update_stats(self):
-        """This function runs every 1hrto automatically update your server count"""
-
+        dbltoken = '{}'.format(os.environ.get("dbltokenT"))
+        headers = {'Authorization' : dbltoken}
         while True:
-            logger.info('attempting to post server count')
-            try:
-                await self.dblpy.post_server_count()
-                logger.info('posted server count ({})'.format(len(self.bot.guilds)))
-            except Exception as e:
-                logger.exception('Failed to post server count\n{}: {}'.format(type(e).__name__, e))
+            urlbot = 'https://discordbots.org/api/bots/385681784614027265/stats'
+            payload = {'server_count': len(bot.guilds)}
+            async with aiohttp.ClientSession() as session:
+                async with session.post(urlbot, data = json.dumps(payload), headers = headers) as r:
+                    if r.status == 200:
+                        pass
+                await session.close()
             await asyncio.sleep(3600)
 
 
