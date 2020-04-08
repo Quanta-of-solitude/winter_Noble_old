a83a14ee38f208819f3329651134cf1719859d8a
Sanjeev Kumar Bharadwaj <34452724+Quanta-of-solitude@users.noreply.github.com>
2020-04-08 23:43:09 +0530

    t

---
+++ create_items.py
@@ -0,0 +1,20 @@
+import os
+import psycopg2
+
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+cur = conn.cursor()
+
+def create_Tables():
+
+    command = ("""CREATE TABLE welcomeyou (serverid VARCHAR,welcomemsg VARCHAR,channelid VARCHAR,welcometype VARCHAR,welcomeswitch VARCHAR, welcomelinkimage VARCHAR)""")
+    cur.execute(command)
+    command2 = ("""CREATE TABLE leaveyou (serverid VARCHAR,leavemsg VARCHAR,channelid VARCHAR)""")
+    cur.execute(command2)
+    command3 = ("""CREATE TABLE autoroler (serverid VARCHAR,roleid VARCHAR)""")
+
+
+cur.close()
+conn.close()
