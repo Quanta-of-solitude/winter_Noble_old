a48e100782e0ad1ae97e6d55fb7c17b52905d3eb
Sanjeev Kumar Bharadwaj <34452724+Quanta-of-solitude@users.noreply.github.com>
2020-04-09 23:33:00 +0530

    test

---
+++ cogs/newShot.py
@@ -0,0 +1,41 @@
+import os
+import discord
+import requests
+from discord.ext import commands
+import imgkit
+
+class newShot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @property
+    def character_page_link(self):
+        '''getting the character link'''
+        with open("data/config.json") as f:
+            link = json.load(f)
+            if link["char_link"] == "link_here":
+                char_link = os.environ.get("char_link")
+            else:
+                char_link = link["char_link"]
+        return char_link
+
+    @commands.command()
+    async def sendshot(self,ctx, *, args:str = None):
+        try:
+            if args == None:
+                await ctx.send(content = "`Missing name`")
+                return
+            link = self.character_page_link
+            new_text = args.replace(' ','+')
+            link = link+new_text
+            imgkit.from_url(f'{link}', 'out.jpg')
+            await ctx.send(file=discord.File('out.jpg'))
+        except Exception as e:
+            print(e)
+
+
+
+
+def setup(bot):
+    bot.add_cog(newShot(bot))
